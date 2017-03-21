780666807f3e5dddc1ef13644ae43eced587fecb
deemetrius <sm0ke999@yandex.ru>
2017-03-21 05:58:22 +0300

    Update tmr.h

--- tmr.h
+++ tmr.h
@@ -1,1 +1,20 @@
-// го
+enum { max_count = 15 };
+typedef int T;
+
+class dicer
+{
+public:
+	T items[max_count];
+	int count;
+	// конструктор
+	dicer() : count(0) {}
+	// добавить один элемент в массив
+	bool append(T item)
+	{
+		// напиши инструкции
+		return true; // если успешно добавили тогда вернёт true // если нет места тогда false
+	}
+	// очистка
+	void clear()
+	{}
+};
