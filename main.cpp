1b2699ea8e220e4b50b16a5e67b9905c3bcd7021
deemetrius <sm0ke999@yandex.ru>
2017-03-21 21:39:46 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "dicer.h"
+
+void show_all(dicer & d)
+{
+	if( d.count )
+	{
+		auto end = d.items + d.count;
+		for(auto h= d.items; h != end; ++h)
+		{
+			std::cout << (*h) << " ";
+		}	
+	} else {
+		std::cout << "empty";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	dicer d;
+	d.append(4);
+	d.append(2);
+	show_all(d);
+	
+	return 0;
+}
